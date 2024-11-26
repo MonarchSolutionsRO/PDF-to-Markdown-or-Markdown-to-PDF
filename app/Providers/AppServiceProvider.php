36c76a5c3291ae0f5f45f7f2be191254739c2b35
MonarchSolutions <office@monarchsolutions.ro>
2024-11-26 13:07:32 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;  // Add this import
+
 
 class AppServiceProvider extends ServiceProvider
 {
